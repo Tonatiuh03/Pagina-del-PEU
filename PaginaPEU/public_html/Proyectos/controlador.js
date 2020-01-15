23fb68cb918c38e6cfc4445f4882a25a7b7ba291
PEU <PEU@Lupita>
2020-01-15 14:38:59 -0600

    controlador

--- PaginaPEU/public_html/Proyectos/controlador.js
+++ PaginaPEU/public_html/Proyectos/controlador.js
@@ -1,5 +1,35 @@
 /* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Controlador para agregar proyecto 
  */
+
+       
+var form = document.getElementsByTagName('form')[0];
+var nombreRes = document.getElementById('nombreRes');
+var correo = document.getElementById('correo');
+var entidades = document.getElementById('entidades');
+var tel = document.getElementById('entidades');
+var nombreProy = document.getElementById('nombreProy');
+var resumen = document.getElementById('resumen');
+var tipo = docuiment.getElementById('tipo');
+var etapa = document.getElementById('etapa');
+var esperado = document.getElementById('esperado');
+var web = document.getElementById('web');
+var logros = document.getElementById('logros');
+var uniColaboran = document.getElementById('uniColaboran');
+var financiadoras = document.getElementById('financiadoras');
+var monto = document.getElementById('monto');
+var fechaInicio = document.getElementById('fechaInicio');
+var fechaTermino = document.getElementById('fechaTermino');
+var sigEtapa = document.getElementById('sigEtapa');
+var nuevo = document.getElementById('nuevo');
+var colaboradores = document.getElementById('colaboradores');
+var licenciatura = document.getElementById('licenciatura');
+var maestria = document.getElementById('maestria');
+var doctorado = document.getElementById('doctorado');
+var aeroespacial = document.getElementById('aeroespacial');
+var empresas = document.getElementById('empresas');
+var normativa = document.getElementById('normativa');
+var desarrollo = document.getElementById('desarrollo');
+var area = document.getElementById('desarrollo');
+var adscripcion = document.getElementById('adscripcion');
+
